a31a944bb50080ea1aeb05a57c0ac950b283f944
PierreSQS <nomo_messina@web.de>
2022-12-09 00:35:22 +0100

    Sec10_Chap129-Enhance Get Beer-NOK: adaptations to get Beer with Inventory Information

---
+++ src/test/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
@@ -0,0 +1,32 @@
+package guru.springframework.msscbeerservice.services.inventory;
+
+import guru.springframework.msscbeerservice.bootstrap.BeerLoader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Disabled // utility for manual testing
+@SpringBootTest
+class BeerInventoryServiceRestTemplateImplTest {
+
+    @Autowired
+    BeerInventoryService beerInventoryService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getOnhandInventory() {
+        Integer qoh = beerInventoryService.getOnhandInventory(BeerLoader.BEER_1_UUID);
+        assertThat(qoh).isNotZero();
+
+        System.out.println(qoh);
+
+    }
+}
