7435ca4b572c2b0c12c00fc30b37da0a98655dc8
PierreSQS <nomo_messina@web.de>
2023-01-01 14:17:36 +0100

    Sec12_Chap165-Assn-Database Initialization: Solution with Command Line Runner

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -3,6 +3,7 @@ package guru.springframework.msscbeerservice.bootstrap;
 import guru.springframework.msscbeerservice.domain.Beer;
 import guru.springframework.msscbeerservice.repositories.BeerRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -10,6 +11,7 @@ import java.util.UUID;
 /**
  * Modified by Pierrot on 2022-12-08.
  */
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
@@ -17,8 +19,7 @@ public class BeerLoader implements CommandLineRunner {
     public static final String BEER_3_UPC = "0083783375213";
 
     public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
-    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
-    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
+
 
     private final BeerRepository beerRepository;
 
